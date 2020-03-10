2d2532d6af28b1cb9d0653cc38f5224f9bd221ae
Hannah Chang <hchang0223@gmail.com>
2020-03-10 10:17:54 -0700

    Add my_t_test and test

--- R/my_t_test.R
+++ R/my_t_test.R
@@ -1,3 +1,50 @@
 #' t-test
 #'
+#' This function performs one-sample t-tests on vectors of data.
 #'
+#' @param x Numeric vector of data values.
+#' @param alternative Character string of the alternative hypothesis, must be
+#'   either "\code{two.sided}", "\code{less}", or "\code{greater}".
+#' @param mu Numeric of true value of the mean.
+#'
+#' @return A list containing the following:
+#' @return \code{t_stat}, the value of the test statistic.
+#' @return \code{df}, the degrees of freedom for the t-statistic.
+#' @return \code{alternative}, the alternative hypothesis.
+#' @return \code{p_val}, the p-value for the test.
+#'
+#' @examples
+#' my_t_test(1:10, "two.sided", 0)
+#' my_t_test(rnorm(10), "less", 0)
+#'
+#' @export
+my_t_test <- function(x, alternative, mu) {
+  if (alternative != "two.sided" &
+      alternative != "less" &
+      alternative != "greater") {
+    # error message
+    stop("alternative must be char string 'two.sided', 'less', or 'greater'")
+  }
+
+  # sample size n is number of elements in x vector
+  n <- length(x)
+  # degrees of freedom (df) is n - 1
+  df <- n - 1
+  # calculate standard error
+  se <- sd(x) / sqrt(n)
+  # calculate test statistic
+  test_stat <- (mean(x) - mu) / se
+
+  # calculate p-value depending on alternative hypothesis
+  if (alternative == "two.sided") {
+    p_val <- pt(abs(test_stat), df, lower.tail = FALSE) * 2
+  } else if (alternative == "less") {
+    p_val <- pt(test_stat, df)
+  } else {
+    p_val <- pt(test_stat, df, lower.tail = FALSE)
+  }
+
+  result_list <- list(t_stat = test_stat, df = df, alternative = alternative,
+                      p_val = p_val)
+  return(result_list)
+}
--- tests/testthat/test-my_t_test.R
+++ tests/testthat/test-my_t_test.R
@@ -1,1 +1,26 @@
 # within test-my_t_test.R
+test_that("alternative input that isn't 'two.sided', 'less', or 'greater'
+          throws error", {
+  expect_error(my_t_test(1:10, "a string", 0))
+  expect_error(my_t_test(1:10, 5, 0))
+})
+
+test_that("p-value is calculated correctly", {
+  expect_equal(my_t_test(1:10, "two.sided", 0)$p_val, 3e-04,
+               tolerance = 0.001)
+  expect_equal(my_t_test(1:10, "less", 0)$p_val, 0.9999,
+               tolerance = 0.001)
+  expect_equal(my_t_test(1:10, "greater", 0)$p_val, 1e-04,
+               tolerance = 0.001)
+})
+
+test_that("my_t_test outputs a list", {
+  expect_is(my_t_test(1:10, "less", 0), "list")
+})
+
+test_that("elements of output list are numeric, numeric, character, numeric", {
+  expect_is(my_t_test(1:10, "less", 0)$t_stat, "numeric")
+  expect_is(my_t_test(1:10, "less", 0)$df, "numeric")
+  expect_is(my_t_test(1:10, "less", 0)$alternative, "character")
+  expect_is(my_t_test(1:10, "less", 0)$p_val, "numeric")
+})
