64281409298e5442c7e6e758a1bbfd987b7e2802
Hannah Chang <hchang0223@gmail.com>
2020-03-10 19:30:32 -0700

    Add my_lm and test

--- R/my_lm.R
+++ R/my_lm.R
@@ -1,1 +1,49 @@
-#' linear regression model
+#' Fitting Linear Models
+#'
+#' This function is used to fit linear models on a data set.
+#'
+#' @param formula A "\code{formula}" class object that describes the model to
+#'   be fitted, typically having the form \code{response ~ terms}.
+#' @param data Data frame containing the variables in the model.
+#'
+#' @return A table with rows for each coefficient and columns for the following:
+#'   \code{Estimate}, \code{Std. Error}, \code{t value}, and \code{Pr(>|t|)}.
+#'
+#' @examples
+#' # load mtcars data
+#' data("mtcars")
+#'
+#' my_lm(mpg ~ hp, mtcars)
+#' my_lm(mpg ~ hp + wt, mtcars)
+#'
+#' @export
+my_lm <- function(formula, data) {
+  # extract model matrix X
+  matrix_x <- model.matrix(formula, data)
+  # transpose of model matrix X
+  transpose_x <- t(matrix_x)
+  # extract model frame
+  m_frame <- model.frame(formula, data)
+  # extract model response Y using frame
+  response_y <- model.response(m_frame)
+
+  # solve for coefficients (estimate)
+  beta <- solve((transpose_x %*% matrix_x)) %*% transpose_x %*% response_y
+
+  # calculate degrees of freedom (sample size - covariates - intercept)
+  df <- nrow(data) - nrow(beta)
+  # estimate sigma hat squared
+  sigma <- sum((response_y - matrix_x %*% beta)^2 / df)
+  # calculate standard error with sigma squared
+  se <- suppressWarnings(diag(sqrt(sigma * solve((transpose_x %*% matrix_x)))))
+
+  # calculate t value
+  t_val <- beta / se
+
+  # calculate p-value (beta = 0)
+  p_val <- pt(abs(t_val), df, lower.tail = FALSE) * 2
+
+  result_table <- as.table(cbind(beta, se, t_val, p_val))
+  colnames(result_table) <- c("Estimate", "Std. Error", "t value", "Pr(>|t|)")
+  return(result_table)
+}
--- tests/testthat/test-my_lm.R
+++ tests/testthat/test-my_lm.R
@@ -1,1 +1,25 @@
 # within test-my_lm.R
+
+test_that("my_lm outputs a table", {
+  expect_is(my_lm(mpg ~ hp + wt, mtcars), "table")
+})
+
+test_that("estimate for each coefficient is calculated correctly", {
+  expect_equal(my_lm(mpg ~ hp, mtcars)[1, 1], 30.09886, tolerance = 0.0001)
+  expect_equal(my_lm(mpg ~ hp, mtcars)[2, 1], -0.06823, tolerance = 0.0001)
+})
+
+test_that("standard error for each coefficient is calculated correctly", {
+  expect_equal(my_lm(mpg ~ hp, mtcars)[1, 2], 1.63392, tolerance = 0.0001)
+  expect_equal(my_lm(mpg ~ hp, mtcars)[2, 2], 0.01012, tolerance = 0.0001)
+})
+
+test_that("t value for each coefficient is calculated correctly", {
+  expect_equal(my_lm(mpg ~ hp, mtcars)[1, 3], 18.421, tolerance = 0.0001)
+  expect_equal(my_lm(mpg ~ hp, mtcars)[2, 3], -6.742, tolerance = 0.0001)
+})
+
+test_that("p value for each coefficient is calculated correctly", {
+  expect_equal(my_lm(mpg ~ hp, mtcars)[1, 4], 6.6427e-18, tolerance = 0.0001)
+  expect_equal(my_lm(mpg ~ hp, mtcars)[2, 4], 1.79e-07, tolerance = 0.0001)
+})
