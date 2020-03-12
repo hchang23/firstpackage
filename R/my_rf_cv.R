2256567a82070f48a6c778bb8c4056b9f1a2ac89
Hannah Chang <hchang0223@gmail.com>
2020-03-11 17:13:59 -0700

    Add my_rf_cv and test

--- R/my_rf_cv.R
+++ R/my_rf_cv.R
@@ -1,1 +1,48 @@
 #' Random Forest Cross-Validation
+#'
+#' This function uses the \code{my_gapminder} data to predict \code{lifeExp}
+#' using covariate \code{gdpPercap} using folds.
+#'
+#' @import randomForest magrittr
+#'
+#' @param k Integer of the number of folds.
+#' @keywords prediction
+#'
+#' @return A numeric for the cross-validation error.
+#'
+#' @examples
+#' my_rf_cv(5)
+#'
+#' @export
+my_rf_cv <- function(k) {
+  if (is.numeric(k) == FALSE) {
+    stop("k must be numeric")
+  }
+
+  # sample size
+  n <- nrow(my_gapminder)
+  # randomly assign observations to variable folds
+  folds <- sample(rep(1:k, length = n))
+  # empty vector
+  mse <- vector()
+
+  for(i in 1:k) {
+    # x_i
+    data_train <- my_gapminder[folds != i, ]
+    # x_i*
+    data_test <- my_gapminder[folds == i, ]
+    # y_i*
+    cl_test <- my_gapminder$lifeExp[folds == i]
+
+    # random forest model
+    forest_model <- randomForest(lifeExp ~ gdpPercap, data = data_train,
+                                 ntree = 100)
+    # predict lifeExp of each fold
+    pred <- predict(forest_model, data_test[, -4])
+
+    # calculate and add the ith average MSE into vector
+    mse <- c(mse, mean((pred - cl_test)^2))
+    mse <- mean(mse)
+  }
+  return(mse)
+}
--- tests/testthat/test-my_rf_cv.R
+++ tests/testthat/test-my_rf_cv.R
@@ -1,1 +1,8 @@
 # within test-my_rf_cv.R
+test_that("my_rf_cv outputs a numeric", {
+  expect_is(my_rf_cv(5), "numeric")
+})
+
+test_that("non-numeric input throws an error", {
+  expect_error(my_rf_cv("a string"))
+})
